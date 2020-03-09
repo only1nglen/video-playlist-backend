38ad8d28279479bde52660a25623d46b5f499bb7
only1nglen <only1nglen@gmail.com>
2020-03-09 12:02:12 -0400

    Added model and Route, and crud tested for Video

---
+++ app/models/video.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const videoSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Video', videoSchema)
